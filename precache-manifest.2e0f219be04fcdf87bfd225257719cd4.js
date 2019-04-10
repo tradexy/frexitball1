0333cbd93c3421a4de3ad2ddf44058fe7acfb503
John Mosquera <tradexy@gmail.com>
2019-04-10 17:40:55 +0100

    frexitball1 just build folder

---
+++ precache-manifest.2e0f219be04fcdf87bfd225257719cd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7633fa3477dfb3733a7",
+    "url": "/static/js/main.b11e7e9e.chunk.js"
+  },
+  {
+    "revision": "b875505896d81452f67b",
+    "url": "/static/js/2.3610947e.chunk.js"
+  },
+  {
+    "revision": "e7633fa3477dfb3733a7",
+    "url": "/static/css/main.3c9cf2b6.chunk.css"
+  },
+  {
+    "revision": "37eec22d90ca4302229c9a756f26761a",
+    "url": "/index.html"
+  }
+];
